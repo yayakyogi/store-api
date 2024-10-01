86af2e5557d993fc4f33d9ee1790222952b957d4
yayakyogi <yayaktaka@gmail.com>
2024-10-01 15:30:53 +0700

    change username and password postgres test

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :store_api, StoreApi.Repo,
-  username: "yogi23",
-  password: "yogi23",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost",
   database: "store_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
